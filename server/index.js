db58984aefef67608d349eb9ee176a747b22847b
felix-odenhall <90831955+felix-odenhall@users.noreply.github.com>
2022-03-14 11:06:18 +0100

    connected and tested database

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongodb from 'mongodb';
+import cors from 'cors';
 
 const mongoClient = new mongodb.MongoClient('mongodb://localhost:27017');
 
@@ -16,6 +17,37 @@ const PORT = 8080;
 const app = express();
 
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: 'http://localhost:5500',
+  })
+);
+
+app.get('/products', async (req, res) => {
+  const query = req.query;
+  console.log('Query', query);
+  let filter = {};
+  if (query.containsPuppy) {
+    filter.containsPuppy = query.containsPuppy === 'true';
+  }
+  if (query.breed) {
+    filter.breed = { $regex: new RegExp(query.breed, 'i') };
+  }
+
+  console.log('Filter', filter);
+  const dogs = await collection.find(filter).toArray();
+
+  res.json(dogs);
+});
+
+app.post('/products', async (req, res) => {
+  const dogPic = req.body;
+
+  await collection.insertOne(dogPic);
+
+  res.status(200).end();
+});
 
 app.listen(PORT, () => {
   console.log(`Server is up and running @ http://localhost:${PORT}`);
