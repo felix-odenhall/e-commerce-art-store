095738de89d8cbd84e11ddbac64fd5acb19fbec5
felix-odenhall <90831955+felix-odenhall@users.noreply.github.com>
2022-03-17 14:42:19 +0100

    added navbar to contact page

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,6 +1,9 @@
+import Navbar from '../components/Navbar/Navbar';
+
 const Contact = () => {
   return (
     <>
+      <Navbar />
       <h2>Concatc information</h2>
       <h3>Name: Felix Odenhall</h3>
       <h3>Email: felix.odenhall@gmail.com</h3>
