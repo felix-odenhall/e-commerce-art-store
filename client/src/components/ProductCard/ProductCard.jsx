027350538ca10c18cb2c53b1084b414df8d715cf
Felix Odenhall <bilbo@MBPsomthorFelix.localdomain>
2022-05-17 12:41:09 +0200

    fixed warnings

--- client/src/components/ProductCard/ProductCard.jsx
+++ client/src/components/ProductCard/ProductCard.jsx
@@ -1,18 +1,18 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const ProductCard = ({ _id, image, title, type, price }) => {
   return (
     <>
       <div className=" w-56 rounded-md overflow-hidden shadow-lg m-2 bg-white">
         <Link to={`/product/${_id}`}>
-          <img src={image} alt={`A picture of ${title}`} />
+          <img src={image} alt={`A ${title}`} />
         </Link>
         <div className="px-6 py-4">
           <Link to={`/product/${_id}`}>
-            <h3>{title ? title : 'Coming soon'}</h3>
+            <h3>{title ? title : "Coming soon"}</h3>
           </Link>
-          <p>{type ? type : ''}</p>
-          <p>${price ? price : 'Pricless'}</p>
+          <p>{type ? type : ""}</p>
+          <p>${price ? price : "Pricless"}</p>
         </div>
       </div>
     </>
